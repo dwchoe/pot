1ea0df415c7b3c620b197f8471b8cc61b5cda54f
Daewon Choe <daewon.choe@target.com>
2017-05-14 20:07:43 -0500

    updates to kafka

--- springboot/src/main/java/start/service/KafkaProducerService.java
+++ springboot/src/main/java/start/service/KafkaProducerService.java
@@ -11,44 +11,6 @@ import java.util.Properties;
  * Created by z0019mz on 5/14/17.
  */
 public class KafkaProducerService {
-    public static void main(String[] arg) throws InterruptedException {
-        String serializer = "org.apache.kafka.common.serialization.StringSerializer";
-        String keystorePassword = "password";
-        Properties props = new Properties();
-        props.put("bootstrap.servers", "hostname:port");
-        props.put("acks", "all");
-        props.put("retries", Integer.valueOf(0));
-        props.put("batch.size", Integer.valueOf(16384));
-        props.put("linger.ms", Integer.valueOf(1));
-        props.put("buffer.memory", Integer.valueOf(33554432));
-        props.put("client.id", "test-producer");
-        props.put("key.serializer", serializer);
-        props.put("value.serializer", serializer);
-        props.put("security.protocol", "SSL");
-        props.put("ssl.protocol", "TLSv1.2");
-        props.put("ssl.truststore.location", "/Applications/kafka/kafka_2.10-0.10.0.0/ssl-profile/client.truststore.jks");
-        props.put("ssl.truststore.password", keystorePassword);
-        props.put("ssl.keystore.location", "/Applications/kafka/kafka_2.10-0.10.0.0/ssl-profile/server.keystore.jks");
-        props.put("ssl.keystore.password", keystorePassword);
-        Date curDate = new Date();
-        new SimpleDateFormat();
-        String topic = "topic_name";
-        SimpleDateFormat format = new SimpleDateFormat("dd-M-yyyy hh:mm:ssss");
-        String dateToStr = format.format(curDate);
-        KafkaProducer producer = new KafkaProducer(props);
-
-        for(int i = 0; i < 1000; ++i) {
-            try {
-                producer.send(new ProducerRecord(topic, Integer.toString(i), dateToStr + i));
-            } catch (Exception var11) {
-                ;
-            }
-
-            Thread.sleep(100L);
-        }
-
-        producer.close();
-    }
 
     public void sendMessage(String topic) {
         String serializer = "org.apache.kafka.common.serialization.StringSerializer";
